0717afe518ec8be3278decd9e2dfa76721bbc13a
Ruslan <shadmanov.ruslan@gmail.com>
2023-12-12 20:57:17 -0800

    firts part

---
+++ script.js
@@ -0,0 +1,7 @@
+import { cocktails } from "./data.js";
+
+const main = document.querySelector(".main-content");
+const search = document.querySelector("#search");
+const ulList = document.querySelector(".list");
+const clearBtn = document.querySelector(".clear");
+// const searchBtn = document.querySelector("#search-btn");
